794cc658f6b8959b845e7601d938fa8891778b94
trunghuong9920 <trunghuongcdbk@gmail.com>
2021-11-15 20:30:14 +0700

    update

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// set input_search for mobile
+function search_on_mobile(){
+    document.getElementById("header_search-adress").style.display = 'none';
+    document.getElementById("header_cart").style.display = 'none';
+    document.getElementById("header_cart-history").style.display = 'none';
+}
+function show_tab_on_mobile(){
+    document.getElementById("header_search-adress").style.display = 'block';
+    document.getElementById("header_cart").style.display = 'block';
+    document.getElementById("header_cart-history").style.display = 'block';
+}
