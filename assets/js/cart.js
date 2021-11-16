939f0d020be8a2ddcc6d4f4ef992d2e7c24668bf
trunghuong9920 <trunghuongcdbk@gmail.com>
2021-11-16 16:25:59 +0700

    update

---
+++ assets/js/cart.js
@@ -0,0 +1,15 @@
+function show_detail_product_cart(){
+    document.getElementById("hidden_detail_product_cart").style.display="block";
+    document.getElementById("product_header_detail_list").style.display="block";
+
+    document.getElementById("show_detail_product_cart").style.display="none";
+    
+}
+
+function hidden_detail_product_cart(){
+    document.getElementById("hidden_detail_product_cart").style.display="none";
+    document.getElementById("product_header_detail_list").style.display="none";
+
+    document.getElementById("show_detail_product_cart").style.display="block";
+    
+}
