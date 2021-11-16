b2e600181ecd9d5a5783f24d5627b8ba8a1cef97
trunghuong9920 <trunghuongcdbk@gmail.com>
2021-11-16 21:24:54 +0700

    update

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -13,3 +13,28 @@ function hidden_detail_product_cart(){
     document.getElementById("show_detail_product_cart").style.display="block";
     
 }
+
+function choosegetgoods_tab_1(){
+    var tab =document.getElementById("delivery_address_detail");
+    tab.style.display = "block";   
+
+
+    document.getElementById("delivery_product_body_notes").style.display="block";
+
+    document.getElementById("delivery_address_detail_tab_2").style.display = "none";
+
+    document.getElementById("choose-tab-2_cb").checked = false;
+
+}
+
+function choosegetgoods_tab_2(){
+    var tab =document.getElementById("delivery_address_detail");
+    tab.style.display = "none";   
+    var notes = document.getElementById("delivery_address_notes");
+    notes.style.display = "none";    
+    document.getElementById("delivery_address_detail_tab_2").style.display = "block";
+
+    document.getElementById("delivery_product_body_notes").style.display="none";
+
+    document.getElementById("choose-tab-2_cb").checked = true;
+}
